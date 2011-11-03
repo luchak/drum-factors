91c800dde65d15e13049129b8c2663487d4f449c
Matt Stanton <luchak@gmail.com>
2011-11-02 20:23:35 -0400

    Read/write .wav files.

--- drum_factors.py
+++ drum_factors.py
@@ -17,32 +17,69 @@ def ReadWav(filename):
     wav = wave.open(filename, 'r')
     channels = wav.getnchannels()
     sample_bytes = wav.getsampwidth()
-    sample_rate = wave.getframerate()
 
     wav_data = wav.readframes(wav.getnframes())
 
     if (sample_bytes == 2):
-      wav_array = numpy.fromstring(wav_data, dtype=numpy.sint16)
+      wav_array = numpy.fromstring(wav_data, dtype=numpy.int16)
+    elif (sample_bytes == 1):
+      wav_array = numpy.fromstring(wav_data, dtype=numpy.int8)
     else:
-      assert False
+      raise ValueError('Sample depth of %d bytes not supported' % sample_bytes)
+
+    float_array = numpy.zeros(wav_array.shape[0] / channels)
+    for i in range(channels):
+      float_array += wav_array[i::channels]
+    float_array /= max(abs(float_array))
   finally:
     if wav:
       wav.close()
 
+  return float_array
+
+def WriteWav(filename, data, channels=1, sample_rate=44100):
+  wav = None
+  try:
+    wav = wave.open(filename, 'w')
+    wav.setnchannels(channels)
+    wav.setsampwidth(2)
+    wav.setframerate(sample_rate)
+    wav.setnframes(len(data) / channels)
+
+    norm_data = (data * 32767 / (max(abs(data)))).astype(numpy.int16)
+
+    wav.writeframes(norm_data.tostring())
+  finally:
+    if wav:
+      wav.close()
+
+def Sweep(length, start, end):
+  samples = int(SAMPLE_RATE * length)
+
+  fractions = numpy.arange(samples) / float(samples)
+  scale = 2.0 * math.pi / SAMPLE_RATE
+  return numpy.sin(scale * numpy.cumsum(start + (end - start)*fractions))
+
+def Noise(length):
+  samples = int(SAMPLE_RATE * length)
+  return numpy.random.random(samples)
+
+def LinearFadeOut(sound, length=None):
+  if length:
+    samples = int(min(len(sound), SAMPLE_RATE * length))
+  else:
+    samples = len(sound)
+
+  gain = numpy.ones(len(sound))
+  gain[-samples:] = 1.0 - (numpy.arange(samples).astype(numpy.float64) / samples)
+  print gain[::1000]
+
+  return sound * gain
+
+
 def main(argv):
-  wav_array = array.array('h')
-  for i in range(44100):
-    wav_array.append(int(math.sin((330.0 + 220*(i/44100.0)) * 2.0 * math.pi * i / 44100.0) * 16000))
-
-  output_filename = 'test.wav'
-  wav = wave.open(output_filename, 'w')
-  wav.setnchannels(CHANNELS)
-  wav.setsampwidth(SAMPLE_BYTES)
-  wav.setframerate(SAMPLE_RATE)
-
-  wav.setnframes(44100)
-  wav.writeframes(wav_array)
-  wav.close()
+  WriteWav('bd.wav', LinearFadeOut(Sweep(0.25, 200, 60)))
+  WriteWav('noise.wav', Noise(0.05))
 
 if  __name__ == '__main__':
   sys.exit(main(sys.argv))
